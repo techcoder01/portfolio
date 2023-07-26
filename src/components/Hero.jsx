8d62fa69e166f7a0486f660180b0fe1dd3277efd
techcoder01 <techcoderhelp@gmail.com>
2023-07-26 22:00:25 +0500

    Hello World

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,15 +16,16 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Abdul Hannan</span>
+            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
           I specialize in developing <br className='sm:block hidden' />
-          responsive apps using Flutter for Android and iOS platforms.
+          responsive apps using Flutter for  <br className='sm:block hidden' />
+          Android and iOS platforms.
           </p>
         </div>
       </div>
-      
+
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
